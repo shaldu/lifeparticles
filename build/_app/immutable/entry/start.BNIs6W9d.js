8633d7071845f60f751dada5f3ef54cef05380e0
shaldu <mikekrenn11@gmail.com>
2024-03-11 11:56:04 +0100

    changes for githubpage

--- build/_app/immutable/entry/start.BNIs6W9d.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CV3lo7Nt.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+			strict: false,
+		}),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/lifeparticles.github.io' : '',
+        },		
+    },
+
 };
+
+export default config;
